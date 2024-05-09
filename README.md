56978404c57f887a84c999168fe37088ee12f418
Faiza-Mazhar-Ali <rizwanali2k18114@gmainl.com>
2024-05-09 11:17:07 +0500

    This calculator is only based on division

---
+++ README.md
@@ -0,0 +1,1 @@
+"# cli-division-calculator-" 
---
+++ index.js
@@ -0,0 +1,21 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+//first promt 
+let option1 = await inquirer.prompt([
+    {
+        name: "num1",
+        type: "number",
+        message: "please enter your first number:"
+    }
+]);
+//second promt 
+let option2 = await inquirer.prompt([
+    {
+        name: "num2",
+        type: "number",
+        message: "please enter your second number:"
+    }
+]);
+//print output
+let output = option1.num1 / option2.num2;
+console.log(output);
